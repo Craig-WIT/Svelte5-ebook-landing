01006316bbdfbeee213f3b4e207359feff7b810d
Craig-WIT <20095351@mail.wit.ie>
2024-11-30 21:00:59 +0000

    Adding Stripe for payments

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,3 +1,33 @@
+<script>
+  import { loadStripe } from "@stripe/stripe-js";
+  import { PUBLIC_STRIPE_KEY } from "$env/static/public";
+  import {goto} from "$app/navigation"
+
+  let { children, ...props } = $props();
+
+  async function onclick() {
+    try{
+      const stripe = await loadStripe(PUBLIC_STRIPE_KEY);
+
+    const response = await fetch("/api/checkout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const { sessionId } = await response.json();
+
+    await stripe.redirectToCheckout({ sessionId });
+    }
+    catch(error) {
+      goto("/checkout/failure")
+    }
+  }
+</script>
+
+<button {...props} {onclick}>{@render children()} onclick=</button>
+
 <style>
   button {
     background-color: black;
@@ -6,7 +36,7 @@
     font-weight: normal;
     font-size: 22px;
     text-transform: uppercase;
-    transition: all 0.3s;
+    transition: all 0.5s;
     border: 1px solid white;
   }
 
@@ -15,9 +45,3 @@
     color: black;
   }
 </style>
-
-<script>
-  let {children, ...props} = $props()
-</script>
-
-<button {...props}>{@render children()}</button>
